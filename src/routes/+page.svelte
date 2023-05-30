b3ca20340b34a033722d35c4962a0a191a3d3461
Kyler Chin <kyler@kylers-surface>
2023-05-30 01:21:41 -0700

    new svelte maplibre program

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import maplibregl from 'maplibre-gl';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const map = new maplibregl.Map({
+			container: 'map',
+			style:
+				'https://api.maptiler.com/maps/dbb80139-208d-449f-a69e-31243c0ee779/style.json?key=tf30gb2F4vIsBW5k9Msd', // stylesheet location
+			center: [-118, 33.9], // starting position [lng, lat]
+			zoom: 8 // starting zoom
+		});
+	});
+</script>
+
+<div id="map" style="width: 100%; height: 100%;" />
+
+<style>
+	#map {
+		width: 100%;
+		height: 100%;
+	}
+</style>
