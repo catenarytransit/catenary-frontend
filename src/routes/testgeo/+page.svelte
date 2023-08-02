918b330a7fa53d4c9fdfe6dce0c9420be59ad026
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-08-02 00:54:39 -0700

    whatever

--- src/routes/testgeo/+page.svelte
+++ src/routes/testgeo/+page.svelte
@@ -1,19 +1,32 @@
 <script>
     import { onMount } from "svelte";
-    import getLocation from "geolocation";
     
     let lat = 0;
     let lng = 0;
     let speed = 0;
-    let bearing = 0;
+    let heading = 0;
+    let accuracy = 0;
     
     onMount(() => {
-      getLocation().then(({ coords }) => {
-        lat = coords.latitude;
-        lng = coords.longitude;
-        speed = coords.speed;
-        bearing = coords.bearing;
-      });
+        function success(pos) {
+  lat = pos.coords.latitude;
+  lng = pos.coords.longitude;
+  speed = pos.coords.speed;
+  heading = pos.coords.heading;
+  accuracy = pos.coords.accuracy;
+}
+
+function error(err) {
+  console.error(`ERROR(${err.code}): ${err.message}`);
+}
+
+const options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+const id = navigator.geolocation.watchPosition(success, error, options);
     });
     </script>
     
@@ -22,4 +35,5 @@
     <p>Latitude: <b>{{ lat }}</b></p>
     <p>Longitude: <b>{{ lng }}</b></p>
     <p>Speed: <b>{{ speed }}</b></p>
-    <p>Bearing: <b>{{ bearing }}</b></p>
+    <p>Heading: <b>{{ heading }}</b></p>
+    <p>Accuracy: <b>{{accuracy}}</b></p>
