6c79f52f426dd549d0bbb80e9d560cb75d59d1bf
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-08-12 20:28:11 -0700

    fix kmh conversion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -539,7 +539,7 @@ map.addLayer({
 {#if typeof geolocation.coords.speed === "number"} 
 
 
-<div class="inter absolute top-1 left-0 px-1 py-1 bg-white text-black text-sm z-10">{geolocation.coords.speed.toFixed(2)} m/s {geolocation.coords.speed.toFixed(2)} km/h</div>
+<div class="inter absolute top-1 left-0 px-1 py-1 bg-white text-black text-sm z-10">{geolocation.coords.speed.toFixed(2)} m/s {(3.6 * geolocation.coords.speed).toFixed(2)} km/h</div>
 {/if}
 
 {/if}
