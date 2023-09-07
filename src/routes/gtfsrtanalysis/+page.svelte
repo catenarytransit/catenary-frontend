d3253e5b45415ec28ac09ea1498532239014d4fa
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-07 12:52:37 -0700

    gtfs rt validator

---
+++ src/routes/gtfsrtanalysis/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let info:any[] = [];
+
+    let time = Date.now();
+
+    //onload
+
+    onMount(() => {
+        setInterval(() => {
+            
+        }, 500)
+
+        setInterval(() => {
+            time = Date.now()
+        },20)
+
+    })
+</script>
+
+<div>
+    <div>
+        <h1>
+            Kactus GTFS-rt audit and analysis
+        </h1>
+        <p class='slashed-zero tabular-nums'>{time}</p>
+        <p>{new Date(time).toISOString()}</p>
+    </div>
+</div>
