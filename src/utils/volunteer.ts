2f01a37d32d35bff9e8c2aebfb43c98168916a56
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-18 01:17:08 -0700

    valid geolocation

---
+++ src/utils/volunteer.ts
@@ -0,0 +1,32 @@
+//make a socket connection to the server wstransit.kylerchin.com
+//throw submissions at it :)
+
+function isValidGeolocation(geolocation: GeolocationPosition) {
+
+    //throw out any locations with accuracy > 10 meters
+    if (geolocation.coords.accuracy < 10) {
+        return false;
+    }
+
+    //require speed submission because speed is a good indicator of a mobile device
+    //additionally, this ensures that mobile devices are not submitting static locations
+    if (geolocation.coords.speed === null) {
+        return false;
+    }
+
+    //no null values
+    if (geolocation.coords.latitude === null || geolocation.coords.longitude === null) {
+        return false;
+    }
+
+    //no submissions at or around null island
+    if (geolocation.coords.latitude === 0 || geolocation.coords.longitude === 0) {
+        return false;
+    }
+
+    if (Math.abs(geolocation.coords.latitude) < 0.01 && Math.abs(geolocation.coords.longitude) < 0.01) {
+        return false;
+    }
+
+    return true;
+}
