0a2fc8ce9b76e8d40943d5cf5878e4a112fb155a
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-25 05:10:59 -0700

    update our website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,15 @@
     import "../app.css";
   </script>
 
+  <style>
+    body {
+      font-family: 'Noto Sans', sans-serif;
+    }
+
+    p {
+      font-family: 'Noto Sans', sans-serif;
+    }
+  </style>
+
 <!-- Meta Tags Generated with https://metatags.io -->
   <slot />
