53651a3cf02e15b9dd0271bbd4a5ceac93304e7c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-10-05 01:04:00 -0700

    launch maps

---
+++ src/components/maintainers.svelte
@@ -0,0 +1,6 @@
+<MaintainerCard name="Kyler Chin" pronouns="he/him" role="Boardmember, Founder, Director, Systems Programmer, Algorithms, Frontend, Design" school="University of California, Irvine" profilePicture="https://github.com/kylerchin.png" />
+<MaintainerCard name="Kin Tsang" pronouns="he/him" role="Boardmember, Systems Architecture, Algorithms" school="" profilePicture="https://github.com/CatenaryMaps.png" />
+<MaintainerCard name="Andrew Shen" pronouns="he/him" role="Boardmember, Frontend/Flutter" school="University of California, Irvine" profilePicture="https://github.com/CatenaryMaps.png" />
+<MaintainerCard name="Samuel Sharp" pronouns="he/him" role="Frontend/Flutter, Design, UX/UI" school="San Diego High School" profilePicture="https://github.com/samuelbeepdev.png" />
+<MaintainerCard name="Andrew Bustos" pronouns="he/him" role="Frontend/Flutter, User Design" school="California State University, East Bay" profilePicture="https://github.com/CatenaryMaps.png" />
+<MaintainerCard name="Josh Wong" pronouns="he/him" role="Sysadmin, Systems Programmer, Algorithms" school="Santa Monica College &amp; Don Bosco Technical Institute, Rosemead" profilePicture="https://github.com/lolpro11.png" />
