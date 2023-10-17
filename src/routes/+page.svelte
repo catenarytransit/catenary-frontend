7cc59ddb0bb92ee25d022dc9209717870d38eb65
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-10-17 14:31:18 -0700

    centre x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2086,8 +2086,8 @@
 
 {#if realtime_list.includes("f-mts~rt~onebusaway") && alertPopupShown}
 	<div class="fixed bottom-14 left-4 pointer-events-none dark:bg-gray-900 dark:text-gray-50 pointer-events-auto clickable" style:padding="20px" style:border-radius="10px" style:box-shadow="0 0 10px #bcd52e" style:color="white">
-		<div on:click={() => alertPopupShown = false } style:cursor="pointer" class='border border-gray-500 bg-gray-700 rounded-full h-8 w-8 absolute right-2 top-2'>
-			<span class="material-symbols-outlined margin-auto">
+		<div on:click={() => alertPopupShown = false } style:cursor="pointer" class='border border-gray-500 bg-gray-700 rounded-full h-8 w-8 absolute right-2 top-2  flex justify-center items-center'>
+			<span class="material-symbols-outlined margin-auto select-none">
 				close
 				</span>
 		</div>
