ecd5db978a0d8efa82cb9fb9fb92131ac16d7813
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-12-10 18:16:00 -0800

    change id

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2274,11 +2274,11 @@ on:keydown={() => {
 				fpsmode = !fpsmode;
 			}}
 			checked={usunits}
-			id="us-units"
+			id="FPS"
 			type="checkbox"
 			class="align-middle my-auto w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
 		/>
-		<label for="us-units" class="ml-2">Show FPS</label>
+		<label for="FPS" class="ml-2">Show FPS</label>
 	</div>
 	
 	<div>
