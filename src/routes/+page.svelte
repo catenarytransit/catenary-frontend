ec989617770f6ece56b7afbdc88c17163669af49
lolpro11 <lolpro11lolx@gmail.com>
2023-12-30 04:12:20 -0800

    add removealllive func

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -909,6 +909,16 @@
 	return coordinates;
 	}
 
+	function removealllive(map: any, layerspercategory: any) {
+		map.removeLayer(layerspercategory.bus.livedots);
+		map.removeLayer(layerspercategory.bus.labeldots);
+		map.removeLayer(layerspercategory.other.livedots);
+		map.removeLayer(layerspercategory.other.labeldots);
+		map.removeLayer(layerspercategory.localrail.livedots);
+		map.removeLayer(layerspercategory.localrail.labeldots);
+		map.removeLayer(layerspercategory.intercityrail.livedots);
+		map.removeLayer(layerspercategory.intercityrail.labeldots);
+	}
 	function runSettingsAdapt() {
 		 console.log('run settings adapt', layersettings);
 		if (mapglobal) {
@@ -2273,6 +2283,7 @@ on:keydown={() => {
 						let agencySelect = document.querySelector('#agencySelect').value;
 						if (agencySelect !== 'none') {
 							window.localStorage.setItem('agencySelect', agencySelect);
+							removealllive(mapglobal, layerspercategory);
 							realtime_list = [agencySelect];
 						}
 					}}
