6c52054dfa7a91247f1b19095603b86e947e1916
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-01 08:22:26 -0800

    remove octa proxy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1931,10 +1931,6 @@
 							url = url + '&bodyhash=' + rtFeedsHashVehicles[realtime_id];
 						}
 
-						if (realtime_id == "f-octa~rt") {
-							url = "https://kylerchin.com/octavehicleproxy"
-						}
-
 						let listhas = true;
 
 						if (fetchedavaliablekactus == true && !avaliablerealtimevehicles.has(realtime_id)) {
