d3e71626a0c0e290fe3a7943649b5647066fa3ce
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-02 12:53:57 -0800

    reverse filter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -484,7 +484,7 @@
 							entity.vehicle?.position !== null && entity.vehicle?.position !== undefined
 					)
 					//no vehicles older than 1 hour
-					.filter((entity: any) => entity.vehicle?.timestamp < (Date.now() / 1000) - 3600)
+					.filter((entity: any) => entity.vehicle?.timestamp > (Date.now() / 1000) - 3600)
 					.map((entity: any) => {
 						const { id, vehicle } = entity;
 						//default to bus type
