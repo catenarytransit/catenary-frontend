a1f8b2d0b4c9e18a43445d6f4feec4ed1070067c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-02 13:10:24 -0800

    handle negative numbers

--- src/utils/isoelapsed.ts
+++ src/utils/isoelapsed.ts
@@ -1,6 +1,14 @@
 export function durationToIsoElapsed(inputms: number):string {
     let remaining_seconds:number = inputms / 1000;
 
+    //negative is false
+    let sign:boolean = true;
+
+    if (remaining_seconds < 0) {
+        remaining_seconds = 0 - remaining_seconds;
+        sign = false;
+    }
+
     let minutes:number = Math.floor(remaining_seconds / 60);
     remaining_seconds = remaining_seconds % 60;
 
@@ -19,6 +27,9 @@ export function durationToIsoElapsed(inputms: number):string {
 
     output = output + ` ${remaining_seconds.toFixed(2)}s`;
     
+    if (sign == false) {
+        output = `- ${output}`;
+    }
 
     return output.trim()
 }
