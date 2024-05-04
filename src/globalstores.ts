25cf5b4ac4a6828f3ed500cadee6d01d4d2fd58f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-05-04 01:18:37 -0700

    lock not saved

--- src/globalstores.ts
+++ src/globalstores.ts
@@ -19,6 +19,7 @@ export const realtime_vehicle_locations_store: Writable<Record<string, Record<st
 export const realtime_vehicle_route_cache_store: Writable<Record<string, Record<string, Record<string, any>>>> = writable({});
 export const realtime_vehicle_route_cache_hash_store: Writable<Record<string, Record<string, number>>> = writable({});
 export const realtime_vehicle_locations_last_updated_store: Writable<Record<string, Record<string, number>>> = writable({});
+export const lock_on_gps_store = writable(false);
 
 export const dark_mode_store = writable(false);
 
