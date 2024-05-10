8cd7a7e791339bd3a76c92739969b1a3d0f95dd0
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-05-10 13:02:24 -0700

    bump version numbe

--- src/components/sidebarInternals.svelte
+++ src/components/sidebarInternals.svelte
@@ -306,7 +306,7 @@
 				</div>
 			</div>
 			<p class="text-sm md:text-base">{$_('clickonanyvehicleorroutegetstarted')}</p>
-			<p class="text-xs md:text-sm">Catenary Maps {$_('softwareversion')} 2024-05-10 12:28Z</p>
+			<p class="text-xs md:text-sm">Catenary Maps {$_('softwareversion')} 2024-05-11 01:02Z</p>
 
 			<div></div>
 
