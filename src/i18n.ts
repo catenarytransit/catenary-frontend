724971a7e56a0238b799a041e70fdf2843966634
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-05-14 23:33:54 -0700

    Fucking i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,7 +5,7 @@ register('de', () => import('./locales/de.json'));
 register('fr', () => import('./locales/fr.json'));
 register('es', () => import('./locales/es.json'));
 register('ko', () => import('./locales/ko.json'));
-register('zh', () => import('./locales/zh.json'));
+register('zh-CN', () => import('./locales/zh.json'));
 register('zh-TW', () => import('./locales/zh.json'));
 
 export function getLocaleStorageOrNav() {
