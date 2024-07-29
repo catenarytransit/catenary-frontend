544187ac1f8c2cc5b2b44404c54a7e30fed79793
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-07-29 12:39:54 -0700

    Update version number

--- src/components/sidebarInternals.svelte
+++ src/components/sidebarInternals.svelte
@@ -409,6 +409,6 @@
 			<p class="text-gray-800 dark:text-gray-300">{$_('comingsoon')}</p>
 
 
-			<p class="text-xs md:text-sm  text-gray-800 dark:text-gray-300">Catenary Maps {$_('softwareversion')} 2024-07-29 11:34Z</p>
+			<p class="text-xs md:text-sm  text-gray-800 dark:text-gray-300">Catenary Maps {$_('softwareversion')} 2024-07-29 19:39Z</p>
 		</div>
 	{/if}
