4230e9970f12d3ad74c6166c74f9f6c5bd8e2f06
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-08-09 02:25:45 -0700

    filter for chateaus that aren't realtime

--- src/components/fetch_realtime_vehicle_locations.ts
+++ src/components/fetch_realtime_vehicle_locations.ts
@@ -36,8 +36,12 @@ export function fetch_realtime_vehicle_locations(
 	}
 
 	const realtime_chateaus_in_frame = get(chateaus_in_frame).filter(
-		(chateau_id) => chateau_to_realtime_feed_lookup[chateau_id] != undefined
+		(chateau_id:string) => {
+			return chateau_to_realtime_feed_lookup[chateau_id].length > 0;
+		}
 	);
+
+	console.log('realtime chateaus in frame',  realtime_chateaus_in_frame );
 
 	//console.log('realtime_chateaus_in_frame', realtime_chateaus_in_frame);
 
