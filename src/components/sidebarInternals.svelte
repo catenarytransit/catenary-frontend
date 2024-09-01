f260a6670a17a11a7cf3858a45d5fbcceedd9a55
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-09-01 06:35:08 -0700

    scrolling fix

--- src/components/sidebarInternals.svelte
+++ src/components/sidebarInternals.svelte
@@ -410,7 +410,7 @@
 
 		<TidbitSidebarCard />
 	{/if}
-	<div class="px-4 sm:px-2 lg:px-4 py-2">
+	<div class="px-4 sm:px-2 lg:px-4 py-2 flex flex-col h-full">
 		<p class="text-sm md:text-base lg:text-lg font-semibold">
 			{$_('clickonanyvehicleorroutegetstarted')}
 		</p>
@@ -418,7 +418,7 @@
 
 		<h2 class="text-base md:text-lg text-gray-800 dark:text-gray-300">{$_('nearbydepartures')} Beta</h2>
 
-		<NearbyDepartures/>
+		<div class='flex flex-col h-full select-text'><NearbyDepartures/></div>
 
 		<p class="text-xs md:text-sm text-gray-800 dark:text-gray-300">
 			Catenary Maps {$_('softwareversion')} 2024-09-01 11:22Z
