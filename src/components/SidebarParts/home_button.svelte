983adbbc154667eaff2984c91d185bc0eaff9dc3
Samuel Sharp <samuelsharp4@gmail.com>
2024-09-20 20:15:10 -0700

    Unified navbar

--- src/components/SidebarParts/home_button.svelte
+++ src/components/SidebarParts/home_button.svelte
@@ -1,27 +1,42 @@
 <script lang="ts">
 	import { data_stack_store, map_pointer_store } from '../../globalstores';
 	import { get } from 'svelte/store';
+	import { SettingsStack, StackInterface } from '../stackenum';
 </script>
 
-<button
-	class="{window.innerHeight < 500 ? 'm-1' : "m-1.5 md:m-2"} h-10 w-10 text-seashore border-seashore border-2 rounded-full flex items-center justify-center"
-	on:click={() => {
-		data_stack_store.set([]);
-		let map = get(map_pointer_store);
+<div class="mt-3 mb-2">
+	<img src="/logo.svg" alt="Catenary" class="h-5 inline align-middle pl-3 mr-2 -translate-y-2" />
+	<button
+		class="text-seashore cursor-pointer mx-1"
+		on:click={() => {
+			data_stack_store.set([]);
+			let map = get(map_pointer_store);
 
-		if (map != null) {
-			//set context layer as empty
-			map.getSource('transit_shape_context').setData({
-				type: 'FeatureCollection',
-				features: []
-			});
+			if (map != null) {
+				//set context layer as empty
+				map.getSource('transit_shape_context')?.setData({
+					type: 'FeatureCollection',
+					features: []
+				});
 
-			map.getSource('stops_context').setData({
-				type: 'FeatureCollection',
-				features: []
+				map.getSource('stops_context')?.setData({
+					type: 'FeatureCollection',
+					features: []
+				});
+			}
+		}}
+		aria-label="Home"
+		><span class="material-symbols-outlined block"> home </span>
+	</button>
+	<button
+		class="text-seashore cursor-pointer mx-2"
+		on:click={() => {
+			data_stack_store.update((x) => {
+				x.push(new StackInterface(new SettingsStack()));
+				return x;
 			});
-		}
-	}}
-	aria-label="Home"
-	><span class="material-symbols-outlined block"> home </span></button
->
+		}}
+		aria-label="Settings"
+		><span class="material-symbols-outlined block"> settings </span>
+	</button>
+</div>
