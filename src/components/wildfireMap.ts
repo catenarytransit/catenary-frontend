b50436857ae488f814c17a5eac936000d8229659
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-11-08 12:20:40 -0800

    evacuate

--- src/components/wildfireMap.ts
+++ src/components/wildfireMap.ts
@@ -7,6 +7,8 @@ export function makeFireMap(map: maplibregl.Map, chateaus_in_frame: Writable<str
 
 	const darkMode = get(dark_mode_store);
 
+	const evacuation_fire_url = "https://services3.arcgis.com/uknczv4rpevve42E/arcgis/rest/services/CA_EVACUATIONS_PROD/FeatureServer/0/query/?spatialRel=esriSpatialRelIntersects&f=geojson&where=SHAPE__Area>0&outFields=*";
+
 	const national_usa_fire_arcgis_url =
 		'https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0/query?where=Shape__Area>0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
 	//  const california_firis_arcgis_url = "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/CA_Perimeters_NIFC_FIRIS_public_view/FeatureServer/0/query?where=OBJECTID>0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
@@ -16,6 +18,12 @@ export function makeFireMap(map: maplibregl.Map, chateaus_in_frame: Writable<str
 		type: 'geojson',
 		//data: "https://stg-arcgisazurecdataprod3.az.arcgis.com/exportfiles-2532-201269/IMSR_Incident_Locations_Most_Recent_View_-7922161599661102971.geojson?sv=2018-03-28&sr=b&sig=iNCmDjs038sig3DJ7jyIM6imAabZl3OH2AITGiWUOVw%3D&se=2024-06-17T04%3A49%3A30Z&sp=r"
 		data: national_usa_fire_arcgis_url
+	});
+
+	map.addSource('evacuation_ca_fire', {
+		type: 'geojson',
+		//data: "https://stg-arcgisazurecdataprod3.az.arcgis.com/exportfiles-2532-201269/IMSR_Incident_Locations_Most_Recent_View_-7922161599661102971.geojson?sv=2018-03-28&sr=b&sig=iNCmDjs038sig3DJ7jyIM6imAabZl3OH2AITGiWUOVw%3D&se=2024-06-17T04%3A49%3A30Z&sp=r"
+		data: evacuation_fire_url
 	});
 	/*
             map.addSource("californiafire", {
@@ -32,16 +40,12 @@ export function makeFireMap(map: maplibregl.Map, chateaus_in_frame: Writable<str
 				})
 				.catch((err) => console.error(err));
 			
-                    fetch(
-						california_firis_arcgis_url 
-					)
-					.then(async (data) => await data.json())
-					.then((cleaned_data:any) => {
-						map.getSource(
-						'californiafire'
-						).setData(cleaned_data)
-					})
-					.catch((err) => console.error(err));
+			fetch(evacuation_fire_url)
+				.then(async (data) => await data.json())
+				.then((cleaned_data: any) => {
+					map.getSource('evacuation_fire').setData(cleaned_data);
+				})
+				.catch((err) => console.error(err));
 		}
 	}, 120_000);
 
@@ -79,67 +83,25 @@ export function makeFireMap(map: maplibregl.Map, chateaus_in_frame: Writable<str
 		},
 		minzoom: 5
 	});
-	
-            map.addLayer({
-				source: "californiafire",
-				id: 'caarcgisfire',
-				type: "fill",
-                filter: [
-                    'all',
-                    ['>',['get', 'poly_DateCurrent'], Date.now()  - (86400_000 * 0.8)],
-                    ["==", ['get', 'type'], "Heat Perimeter"]
-                ],
-				paint: {
-					"fill-color": "#dd4400",
-					"fill-opacity": 0.2,
-				},
-				minzoom: 6
-			});
 
-			map.addLayer({
-				source: "californiafire",
-				id: 'caarcgisfireborder',
-				type: "line",
-                filter: [
-                    'all',
-                    ['>',['get', 'poly_DateCurrent'], Date.now()  - (86400_000 * 0.8)],
-                    ["==", ['get', 'type'], "Heat Perimeter"]
-                ],
-				paint: {
-					"line-color": "#eeaa00",
-					"line-width": 1.5
-				},
-				minzoom: 6
-			});
+	map.addLayer({
+		source: 'evacuation_ca_fire',
+		id: 'evacuation_ca_fire_bounds',
+		type: 'fill',
+		paint: {
+			'fill-color': '#dd3300',
+			'fill-opacity': 0.4
+		},
+		minzoom: 5
+	});
 
 			map.addSource('arcgisfirepoint', {
 				type: 'geojson',
 				data: "https://stg-arcgisazurecdataprod3.az.arcgis.com/exportfiles-2532-201269/IMSR_Incident_Locations_Most_Recent_View_-7922161599661102971.geojson?sv=2018-03-28&sr=b&sig=iNCmDjs038sig3DJ7jyIM6imAabZl3OH2AITGiWUOVw%3D&se=2024-06-17T04%3A49%3A30Z&sp=r"
 				//data: "https://stg-arcgisazurecdataprod3.az.arcgis.com/exportfiles-2532-182272/WFIGS_Interagency_Perimeters_Current_-6544343811762491332.geojson?sv=2018-03-28&sr=b&sig=0Qpq7JG2NWRKLZnEynN%2BgcGPt41fWRNZvWGnaO8%2BZao%3D&se=2024-06-17T04%3A55%3A59Z&sp=r"
 			});
-			
-        
-	
 
-            map.addLayer({
-				source: "californiafire",
-				id: 'caarcgisfirelabel',
-				type: "symbol",
-                filter: [
-                    'all',
-                    ['>',['get', 'poly_DateCurrent'], Date.now() - (86400_000 * 0.8)],
-                    ["==", ['get', 'type'], "Heat Perimeter"],
-                ],
-				paint: {
-					"text-color":  darkMode? "#ffaaaa" : "#aa0000",
-				},
-				layout: {
-					"text-field": ['concat', ['get', 'mission'], " FIRE"],
-					'text-size': 8,
-					'text-font': ['Barlow Medium', 'Arial Unicode MS Bold'],
-				},
-				minzoom: 6
-			});
+			
 
 	map.addLayer({
 		source: 'arcgisfire',
@@ -158,6 +120,21 @@ export function makeFireMap(map: maplibregl.Map, chateaus_in_frame: Writable<str
 			'text-field': ['concat', ['get', 'attr_IncidentName'], ' FIRE'],
 			'text-size': 10,
 			'text-font': ['Barlow Medium']
+		},
+		minzoom: 5
+	});
+
+	map.addLayer({
+		source: 'evacuation_ca_fire',
+		id: 'evacuation_ca_fire_txt',
+		type: 'symbol',
+		paint: {
+			'text-color': darkMode ? '#ccaaaa' : '#cc0000'
+		},
+		layout: {
+			'text-field': ['concat', ['get', 'STATUS'], ''],
+			'text-size': 13,
+			'text-font': ['Barlow Bold']
 		},
 		minzoom: 5
 	});
