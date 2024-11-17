2400cc9cb1b979b09031cfe89f1d414ddfb91900
Kyler Chin <kylerschin@gmail.com>
2024-11-17 03:50:08 -0800

    order by Google language order

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,13 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
-register('en', () => import('./locales/en.json'));
 register('de', () => import('./locales/de.json'));
-register('fr', () => import('./locales/fr.json'));
+register('en', () => import('./locales/en.json'));
 register('es', () => import('./locales/es.json'));
-register('ko', () => import('./locales/ko.json'));
+register('fr', () => import('./locales/fr.json'));
+register('nl', () => import('./locales/nl.json'));
 register('zh-CN', () => import('./locales/zh-CN.json'));
 register('zh-TW', () => import('./locales/zh-TW.json'));
-register('nl', () => import('./locales/nl.json'));
+register('ko', () => import('./locales/ko.json'));
 
 export function getLocaleStorageOrNav() {
 	if (typeof window != 'undefined') {
