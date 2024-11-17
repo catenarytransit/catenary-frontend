bd7c290467e130347eaf61c79eb5a844c1014c38
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-11-17 04:06:11 -0800

    null check desc text

--- src/components/serviceAlerts.svelte
+++ src/components/serviceAlerts.svelte
@@ -36,19 +36,21 @@ $: languagelist = Object.values(alerts).map((alert) => {
 							
 							{#each alert.header_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true) as each_header_translation_obj}
 								<p class="text-sm font-bold">{each_header_translation_obj.text.replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"")}</p>
+								{#if alert.description_text}
 								{#each alert.description_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true).filter((x) => x.language == each_header_translation_obj.language) as description_alert}
-									<div class="leading-none">
-										{#each description_alert.text.split('\n') as each_desc_line}
-											<div class="text-sm">
-												{@html each_desc_line.replaceAll(
-													'<a ',
-													'<a target="_blank" class="text-sky-500 dark:text-sky-300 underline"'
-												).replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"").replaceAll(/(\[)?accessibility icon(\])?/g, "<span class=\"bg-blue-200 dark:bg-gray-500 w-3 h-3 rounded-full inline\"><span class=\"text-sm material-symbols-outlined \">accessible</span></span>")}
-											</div>
-										{/each}
-										
-									</div>
-								{/each}
+								<div class="leading-none">
+									{#each description_alert.text.split('\n') as each_desc_line}
+										<div class="text-sm">
+											{@html each_desc_line.replaceAll(
+												'<a ',
+												'<a target="_blank" class="text-sky-500 dark:text-sky-300 underline"'
+											).replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"").replaceAll(/(\[)?accessibility icon(\])?/g, "<span class=\"bg-blue-200 dark:bg-gray-500 w-3 h-3 rounded-full inline\"><span class=\"text-sm material-symbols-outlined \">accessible</span></span>")}
+										</div>
+									{/each}
+									
+								</div>
+							{/each}
+								{/if}
 							{/each}
 						</div>
 						<hr/>
