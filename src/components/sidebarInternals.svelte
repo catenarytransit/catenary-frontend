42eb26dfb93769aad17f6ca652147b581b577842
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-11-17 03:56:33 -0800

    if show gtfs, not store

--- src/components/sidebarInternals.svelte
+++ src/components/sidebarInternals.svelte
@@ -180,7 +180,7 @@
 									class="px-1 py-0.5 md:px-2 md:py-2 bg-gray-100 dark:bg-darksky hover:bg-blue-100 hover:dark:bg-hover text-sm md:text-base leading-snug rounded-lg"
 								>
 								{
-									#if show_gtfs_ids_store
+									#if show_gtfs_ids
 								}
 								<p>
 									<span class="font-mono text-xs dark:text-gray-400 text-gray-500"
