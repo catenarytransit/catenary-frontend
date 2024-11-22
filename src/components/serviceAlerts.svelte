f6781e7d4a9844c5a7a67976e5220e5854a8f00a
Samuel Sharp <samuelsharp4@gmail.com>
2024-11-21 22:07:56 -0800

    fix: clean up lines and text styles in alert UI

--- src/components/serviceAlerts.svelte
+++ src/components/serviceAlerts.svelte
@@ -12,16 +12,18 @@ $: languagelist = Object.values(alerts).map((alert) => {
 }).flat().filter((x, i, a) => a.indexOf(x) == i);
 </script>
 {#if Object.keys(alerts).length > 0}
-				<div class="border-[#F99C24] border-2 leading-snug mb-3 p-2 rounded-md">
-					<img src="/icons/service_alert.svg" alt="(i)" class="h-6 w-6 inline mr-1" />
-					<span class="text-[#F99C24] font-medium"
+				<div class="border-[#F99C24] border leading-snug mb-3 p-2 rounded-md">
+					<img src="/icons/service_alert.svg" alt="(i)" class="h-6 w-6 inline mr-2" />
+					<span class="text-[#F99C24] font-semibold text-lg align-middle"
 						>Service Alert{Object.keys(alerts).length > 1 ? 's' : ''}</span
 					>
+					<div class="py-0.5"></div>
 					{#each Object.values(alerts) as alert}
 						<div class="pt-1">
-							<p class="text-sm font-semibold text-orange-500">
+							<hr class="border-[#F99C24] border-0.5 rounded-xl" />
+							<p class="text-base font-medium text-[#F99C24] pt-1">
 								<span class="">{$_(cause_id_str(alert.cause))}</span>
-								<span> ⇾ </span>
+								<span> // </span>
 								<span>{$_(effect_id_str(alert.effect))}</span>
 								
 							</p>
@@ -35,12 +37,12 @@ $: languagelist = Object.values(alerts).map((alert) => {
 							{/if}
 							
 							{#each alert.header_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true) as each_header_translation_obj}
-								<p class="text-sm font-bold">{each_header_translation_obj.text.replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"")}</p>
+								<p class={`text-sm`}>{each_header_translation_obj.text.replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"")}</p>
 								{#if alert.description_text}
 								{#each alert.description_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true).filter((x) => x.language == each_header_translation_obj.language) as description_alert}
 								<div class="leading-none">
 									{#each description_alert.text.split('\n') as each_desc_line}
-										<div class="text-sm">
+										<div class="text-xs pt-0.5">
 											{@html each_desc_line.replaceAll(
 												'<a ',
 												'<a target="_blank" class="text-sky-500 dark:text-sky-300 underline"'
@@ -53,7 +55,6 @@ $: languagelist = Object.values(alerts).map((alert) => {
 								{/if}
 							{/each}
 						</div>
-						<hr/>
 					{/each}
 				</div>
 			{/if}
