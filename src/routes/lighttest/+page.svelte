018dbf2fed22d1b144ed90acdd4877c25ecb528b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-11-29 22:42:48 -0800

    darken streams

---
+++ src/routes/lighttest/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import maplibregl from 'maplibre-gl';
+    import 'maplibre-gl/dist/maplibre-gl.css';
+    import type {ProjectionSpecification} from 'maplibre-gl';
+    
+    import {onMount} from 'svelte';
+
+    onMount(() => {
+        const map = new maplibregl.Map({
+          container: 'map',
+          hash: 'pos',
+		  pixelRatio: window.devicePixelRatio * 1.4,
+          style: '/light-style.json', // stylesheet location
+          center: [-74.5, 40], // starting position [lng, lat]
+          zoom: 9 // starting zoom
+        });
+
+       map.on('load', () => {
+        map.setProjection({type: "globe"});
+
+        console.log(map);
+       })
+    });
+
+    
+    </script>
+    
+    <div class="w-full">
+<div id="map" class="fixed top-0 left-0 w-[100vw] h-[100vh]" />
+</div>
+
