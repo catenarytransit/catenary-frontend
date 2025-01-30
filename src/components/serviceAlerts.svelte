ff28dd2f45d277c9c8f44455140d04fd995e6023
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-01-29 20:25:14 -0800

    reduce alert spam

--- src/components/serviceAlerts.svelte
+++ src/components/serviceAlerts.svelte
@@ -35,24 +35,31 @@ $: languagelist = Object.values(alerts).map((alert) => {
 									</p>
 								{/each}
 							{/if}
+
+							{#each languagelist.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true) as language}
+								
+								{#each alert.header_text.translation.filter((x) => x.language == language) as each_header_translation_obj}
+									<p class={`text-sm`}>{each_header_translation_obj.text.replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"")}</p>
+								{/each}
+
+								{#each alert.description_text.translation.filter((x) => x.language == language) as description_alert}
+									<div class="leading-none">
+										{#each description_alert.text.split('\n') as each_desc_line}
+											<div class="text-xs pt-0.5">
+												{@html each_desc_line.replaceAll(
+													'<a ',
+													'<a target="_blank" class="text-sky-500 dark:text-sky-300 underline"'
+												).replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"").replaceAll(/(\[)?accessibility icon(\])?/g, "<span class=\"bg-blue-200 dark:bg-gray-500 w-3 h-3 rounded-full inline\"><span class=\"text-xs material-symbols-outlined \">accessible</span></span>")}
+											</div>
+										{/each}
+									</div>
+								{/each}
+								
+
 							
-							{#each alert.header_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true) as each_header_translation_obj}
-								<p class={`text-sm`}>{each_header_translation_obj.text.replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"")}</p>
-								{#if alert.description_text}
-								{#each alert.description_text.translation.filter((x) => languagelist.includes("en-html") ? (x.language != "en") : true).filter((x) => x.language == each_header_translation_obj.language) as description_alert}
-								<div class="leading-none">
-									{#each description_alert.text.split('\n') as each_desc_line}
-										<div class="text-xs pt-0.5">
-											{@html each_desc_line.replaceAll(
-												'<a ',
-												'<a target="_blank" class="text-sky-500 dark:text-sky-300 underline"'
-											).replaceAll(/\<(\/)?p\>/g,"").replaceAll(/\<(\/)?b\>/g,"").replaceAll(/(\[)?accessibility icon(\])?/g, "<span class=\"bg-blue-200 dark:bg-gray-500 w-3 h-3 rounded-full inline\"><span class=\"text-xs material-symbols-outlined \">accessible</span></span>")}
-										</div>
-									{/each}
-									
-								</div>
-							{/each}
-								{/if}
+							
+							
+								
 							{/each}
 						</div>
 					{/each}
