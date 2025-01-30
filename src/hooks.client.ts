3e3f50970ed44e9c6ee1b035f6d1045e5dbec61f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-01-29 23:33:52 -0800

    add canvas replay

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,4 +1,4 @@
-import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+import { handleErrorWithSentry, replayIntegration, replayCanvasIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
@@ -8,14 +8,14 @@ Sentry.init({
 
 	// This sets the sample rate to be 10%. You may want this to be 100% while
 	// in development and sample at a lower rate in production
-	replaysSessionSampleRate: 0.3,
+	replaysSessionSampleRate: 0.1,
 
 	// If the entire session is not sampled, use the below sample rate to sample
 	// sessions when an error occurs.
 	replaysOnErrorSampleRate: 1.0,
 
 	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [replayIntegration()]
+	integrations: [replayIntegration(),replayCanvasIntegration()]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
