b37c069f77ed733b524211c5630e7de5ca29882a
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-01-30 02:07:19 -0800

    add vite plugin for sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
 	plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
 				project: 'javascript-sveltekit'
 			}
 		}),
+		sentryVitePlugin({
+			org: process.env.SENTRY_ORG,
+			project: process.env.SENTRY_PROJECT,
+			authToken: process.env.SENTRY_AUTH_TOKEN
+		  }),
 		sveltekit()
 	],
 	ssr: {
