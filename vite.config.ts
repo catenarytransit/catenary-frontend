e34ef88fdf09df8190307dccfbee92fdb0178d72
Kyler Chin <kyler@catenarymaps.org>
2025-06-07 09:19:58 -0700

    delete sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
 	plugins: [
-		sentryVitePlugin({
-			org: process.env.SENTRY_ORG,
-			project: process.env.SENTRY_PROJECT,
-			authToken: process.env.SENTRY_AUTH_TOKEN
-		  }),
 		sveltekit(),
 		tailwindcss()
 	],
