e33d3e58025385b04a400b9d59d3c5b02a060672
Kyler Chin <kyler@catenarymaps.org>
2025-06-07 13:28:19 -0700

    format code

--- src/components/StopScreen.svelte
+++ src/components/StopScreen.svelte
@@ -2,126 +2,146 @@
 	import { onMount } from 'svelte';
 	import HomeButton from './SidebarParts/home_button.svelte';
 
-    export let chateau: string;
-    export let stop_id: string;
+	import { writable, get } from 'svelte/store';
+	import {
+		data_stack_store,
+		on_sidebar_trigger_store,
+		realtime_vehicle_locations_last_updated_store,
+		realtime_vehicle_locations_store,
+		realtime_vehicle_route_cache_hash_store,
+		realtime_vehicle_route_cache_store,
+		lock_on_gps_store,
+		show_seconds_store,
+		usunits_store,
+		show_zombie_buses_store,
+		show_my_location_store,
+		custom_icons_category_to_layer_id,
+		map_pointer_store,
+		show_gtfs_ids_store,
+		ui_theme_store,
+		stops_to_hide_store
+	} from '../globalstores';
+	export let chateau: string;
+	export let stop_id: string;
 	import TimeDiff from './TimeDiff.svelte';
-    
+
 	import DelayDiff from './DelayDiff.svelte';
-    import {_} from "svelte-i18n";
+	import { _ } from 'svelte-i18n';
 	import Clock from './Clock.svelte';
-    let events_filtered = [];
 
-    let current_time = 0;
+	let show_seconds = get(show_seconds_store);
 
-    let interval_fetch: NodeJS.Timeout | null = null;
-    let data_from_server = null;
+	show_seconds_store.subscribe((value) => {
+		show_seconds = value;
+	});
 
-    function fetch_stop_data() {
-        console.log('Fetching data for chateau:', chateau, 'stop_id:', stop_id);
-      
+	let events_filtered = [];
 
-        fetch("https://birch.catenarymaps.org/departures_at_stop?stop_id=" + stop_id + "&chateau_id=" + chateau, {
-            mode: 'cors'
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Fetched data:', data);
+	let current_time = 0;
 
-                data_from_server = data;
+	let interval_fetch: NodeJS.Timeout | null = null;
+	let data_from_server = null;
 
-               
+	function fetch_stop_data() {
+		console.log('Fetching data for chateau:', chateau, 'stop_id:', stop_id);
 
-                if (data.events) {
-                    events_filtered = data_from_server.events.filter((event) => (event.realtime_departure || event.scheduled_departure) > ((Date.now() / 1000) - 600))
+		fetch(
+			'https://birch.catenarymaps.org/departures_at_stop?stop_id=' +
+				stop_id +
+				'&chateau_id=' +
+				chateau,
+			{
+				mode: 'cors'
+			}
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log('Fetched data:', data);
 
-                   // events_filtered.sort((a,b) => (a.realtime_departure || a.scheduled_departure) - (b.realtime_departure || b.scheduled_departure))
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            })
-    }
+				data_from_server = data;
 
-    onMount(() => {
-        fetch_stop_data();
-        
-        current_time = Date.now();
+				if (data.events) {
+					events_filtered = data_from_server.events.filter(
+						(event) =>
+							(event.realtime_departure || event.scheduled_departure) > Date.now() / 1000 - 600
+					);
 
-        setInterval(() => {
-             current_time = Date.now();
-        }, 500);
+					// events_filtered.sort((a,b) => (a.realtime_departure || a.scheduled_departure) - (b.realtime_departure || b.scheduled_departure))
+				}
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+			});
+	}
 
-        interval_fetch = setInterval(() => {fetch_stop_data()}, 10000);
+	onMount(() => {
+		fetch_stop_data();
 
-        () => {
-            if (interval_fetch) {
-                clearInterval(interval_fetch);
-            }
-        }
-    });
+		current_time = Date.now();
 
-    $: if (chateau || stop_id) fetch_stop_data();
-    
-    </script>
+		setInterval(() => {
+			current_time = Date.now();
+		}, 500);
 
-<div class='h-full'>
-    <HomeButton />
-    	<div class=" catenary-scroll overflow-y-auto pb-64 h-full pr-2">
+		interval_fetch = setInterval(() => {
+			fetch_stop_data();
+		}, 10000);
+
+		() => {
+			if (interval_fetch) {
+				clearInterval(interval_fetch);
+			}
+		};
+	});
+
+	$: if (chateau || stop_id) fetch_stop_data();
+</script>
+
+<div class="h-full">
+	<HomeButton />
+	<div class=" catenary-scroll overflow-y-auto pb-64 h-full pr-2">
 		<div class="flex flex-col">
-		<div>
-            {#if data_from_server}
+			<div>
+				{#if data_from_server}
+					{#if events_filtered}
+						{#each events_filtered as event}
+							<div class="mx-1 my-2 border-b-1 border-gray-500">
+								<p>{event.headsign}</p>
 
-            {#if events_filtered} 
+								<div class="flex flex-row">
+									Departure:
+									<TimeDiff
+										large={false}
+										show_brackets={false}
+										show_seconds={false}
+										diff={(event.realtime_departure || event.scheduled_departure) -
+											current_time / 1000}
+									/>
 
+									<div class={`ml-auto`}>
+										<Clock
+											timezone={data_from_server.primary.timezone}
+											time_seconds={event.realtime_departure || event.scheduled_departure}
+											{show_seconds}
+										/>
+									</div>
+								</div>
 
-                {#each events_filtered as event }
+								{#if event.platform_string_realtime}
+									<p>{event.platform_string_realtime}</p>
+								{/if}
 
-                    <div class="mx-1 my-2 border-b-1 border-gray-500">
-
-
-<p>{event.headsign}</p>
-
-                      <div class="flex flex-row">
-                          Departure:
-                        <TimeDiff
-													large={false}
-													show_brackets={false}
-													show_seconds={false}
-													diff={(event.realtime_departure || event.scheduled_departure) -
-														current_time / 1000}
-												/>
-
-                                                
-                                                <div class={`ml-auto`}>
-                                                    <Clock
-                                                    timezone = {data_from_server.primary.timezone}
-                                                    time_seconds = {event.realtime_departure || event.scheduled_departure}
-                                                    />
-                                                </div>
-                                                    </div>
-
-                    {#if event.platform_string_realtime} 
-
-                    <p>{event.platform_string_realtime} </p>
-                    {/if}
-
-                    {#if event.vehicle_number} 
-                    <p>{$_("vehicle")}: {event.vehicle_number}</p>
-                    {/if}
-
-                    </div>
-                    <br/>
-
-                {/each}
-
-               
-
-                
-            {/if}
-
-        {:else}
-            <p>Loading...</p>
-        {/if}
-        </div>
-    </div>
-</div></div>
+								{#if event.vehicle_number}
+									<p>{$_('vehicle')}: {event.vehicle_number}</p>
+								{/if}
+							</div>
+							<br />
+						{/each}
+					{/if}
+				{:else}
+					<p>Loading...</p>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
