93d8cf6b0795012da8883fcbe50d8b0173d53013
Samuel Sharp <samuelsharp4@gmail.com>
2025-06-25 12:17:27 -0700

    404 page style updates

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,53 +1,60 @@
 <script lang="ts">
 	import '../app.css';
 
-    import { page } from '$app/state';
+	import { page } from '$app/state';
 </script>
 
-    <div class="flex flex-col max-w-md mx-auto h-screen justify-center items-center">
-        <img src="/logo.svg" alt="Catenary Maps" class="logo w-48">
-    
-    <div class="flex flex-col">
-        <h1 class="error-code text-center">404</h1>
-        <p class="message text-center">
-            Looks like you&apos;ve stumbled upon a ghost station. Spooky.
-        </p>
-        <a href="https://maps.catenarymaps.org" class=" mx-auto text-center home-button">Return to Central Station</a>
-    </div>
+<title>Catenary Maps</title>
 
-    </div>
+<div class="flex flex-col max-w-md mx-auto h-screen justify-center items-center">
+	<img src="/logo.svg" alt="Catenary Maps" class="logo w-24" />
 
+	<div class="flex flex-col">
+		<h1 class="error-code text-center">404</h1>
+		<p class="message text-center">Looks like you&apos;ve stumbled upon a ghost station. Spooky.</p>
+		<a href="https://maps.catenarymaps.org" class="mx-auto text-center home-button"
+			>Return to Central Station</a
+		>
+	</div>
+</div>
 
-    <style>
-         .error-code {
-            font-size: 8rem;
-            font-weight: 700;
-            color: #2d4059;
-            margin-bottom: -10px;
-        }
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-        .message {
-            font-size: 1.5rem;
-            color: #2d4059;
-            margin-bottom: 30px;
-            max-width: 600px;
-        }
+	* {
+		font-family: 'Barlow', sans-serif;
+	}
 
-        .home-button {
-            background: #e74c3c;
-            color: white;
-            padding: 15px 40px;
-            border-radius: 30px;
-            text-decoration: none;
-            font-weight: 600;
-            transition: transform 0.2s, box-shadow 0.2s;
-            border: none;
-            cursor: pointer;
-        }
+	.error-code {
+		font-size: 6rem;
+		font-weight: 600;
+		margin-bottom: -10px;
+        color: #00ab9b;
+	}
 
-        .home-button:hover {
-            transform: translateY(-2px);
-            box-shadow: 0 5px 15px rgba(231, 76, 60, 0.3);
-        }
+	.message {
+		font-size: 1.4rem;
+		margin-bottom: 30px;
+		max-width: 600px;
+	}
 
-    </style>
+	.home-button {
+		background: #f3f3f3;
+		color: black;
+		padding: 10px 20px;
+		border-radius: 5px;
+		text-decoration: none;
+		font-weight: 500;
+		font-size: 1.2rem;
+		transition:
+			background-color 0.2s,
+			color 0.2s;
+		border: none;
+		cursor: pointer;
+	}
+
+	.home-button:hover {
+		background-color: #00ab9b;
+		color: white;
+	}
+</style>
