ac657de8fc265cbb70874c737e9e19cde1733602
Julia <77413091+JulianBarbera@users.noreply.github.com>
2025-08-17 16:17:43 -0700

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+          ];
+        };
+      }
+    );
+}
