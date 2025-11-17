22e3fa00079a4c06e9c703d235836f62be01285e
Kyler Chin <kyler@catenarymaps.org>
2025-11-17 14:56:56 -0800

    add vite pwa updater plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,24 @@ import { defineConfig } from 'vitest/config';
 import { execSync } from 'child_process';
 
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	preprocess: vitePreprocess(),
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [sveltekit(), tailwindcss(),
+		VitePWA({
+      // This is the key setting
+      registerType: 'autoUpdate',
+      
+      // These options are often implied by 'autoUpdate' 
+      // but are good to include explicitly.
+      // This ensures the new service worker activates immediately.
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+	],
 	server: {
 		fs: {
 			allow: ['../dist']
